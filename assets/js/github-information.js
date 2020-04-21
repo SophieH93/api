64c974451a6c56ea4a1ac3b09a5a713936656b54
Sophie <sophiehickey123@gmail.com>
2020-04-21 18:04:44 +0000

    added repo data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -30,10 +30,13 @@ function fetchGitHubInformation(event) {
 
     $.when(
         $.getJSON(`https://api.github.com/users/${username}`)
+        $.gtJSON(`https://api.github.com/users.${username}/repos`)
     ).then(
-        function(response) {
-            var userData = response;
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var reopData = secondResponse[0];
             $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
         },
         function(errorResponse) {
             if (errorResponse.status === 404) {
